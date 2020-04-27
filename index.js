1c0928766844270902d588f9b66695d53299644c
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2020-04-28 01:18:27 +0200

    updated en

--- index.js
+++ index.js
@@ -10,4 +10,11 @@ app.get('/nb', (req, res) => {
     res.setHeader('Set-Cookie', `lang=nb; HttpOnly; Path=/`)
       res.end()
   })
+
+app.get('/en', (req, res) => {
+    const inAYear = new Date()
+    inAYear.setTime(inAYear.getTime() + 60*60*24*365)
+    console.log(inAYear)
+    res.setHeader('Set-Cookie', `lang=en; HttpOnly; Path=/`)
+      res.end()
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
