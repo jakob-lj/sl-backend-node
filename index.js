b2b9208784a29678f4c7f62133bd02e69eed4a20
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2020-04-27 19:49:26 +0200

    added routes

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/nb', (req, res) => {
+    const inAYear = new Date()
+    inAYear.setTime(inAYear.getTime() + 60*60*24*365)
+    console.log(inAYear)
+    res.setHeader('Set-Cookie', `lang=nb; HttpOnly; Path=/`)
+      res.end()
+  })
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
