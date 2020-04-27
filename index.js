b3cab5958130b7061ac592c10ce19911abfe5323
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2020-04-28 01:33:28 +0200

    added cors

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+var cors = require('cors')
+app.use(cors())
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/nb', (req, res) => {
     const inAYear = new Date()
